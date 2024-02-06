2e2f1f97b11aa4eaaf6e947576ffb2a93f6735c5
David Pitman <david.pitman@cloudouble.com>
2024-02-06 15:34:30 +1000

    building

---
+++ package.js
@@ -0,0 +1,14 @@
+export default {
+    context: {},
+    gateways: {},
+    helpers: {},
+    loaders: {},
+    namespaces: {
+        lit: "./components"
+    },
+    options: {},
+    regexp: {},
+    templates: {},
+    transforms: {},
+    types: {}
+}
